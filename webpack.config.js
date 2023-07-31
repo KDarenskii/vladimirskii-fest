8aa8db55cc44e7eef27fd53b1e7f45259805b21c
Kirill Darenskii <kdarenskii@mail.ru>
2023-07-31 23:37:27 +0300

    fixed and refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,91 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
 const isDev = !isProd;
 
-const filename = (ext) => (isDev ? `main.${ext}` : `main.[contenthash].${ext}`);
-const assetFilename = () => (isDev ? "[name][ext]" : "[name].[hash][ext]");
+module.exports = {
+    mode: isDev ? "development" : "production",
+    devtool: isDev ? "source-map" : undefined,
+    entry: "./src/index.tsx",
+    output: {
+        filename: `static/js/${getFilename("js")}`,
+        path: path.resolve(__dirname, "build"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+            filename: "index.html",
+            publicPath: "/",
+        }),
+        new MiniCssExtractPlugin({
+            filename: `static/css/${getFilename("css")}`,
+        }),
+    ],
+    module: {
+        rules: initRules(),
+    },
+    optimization: {
+        minimizer: initMinimizer(),
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        plugins: [
+            new TsconfigPathsPlugin({
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            }),
+        ],
+    },
+    devServer: {
+        hot: true,
+        port: 3000,
+        open: true,
+        client: {
+            logging: "error",
+        },
+        historyApiFallback: true,
+    },
+};
 
-function initStyleLoaders(config) {
+function initRules() {
+    return [
+        {
+            test: /\.(ts)|(tsx)$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+        },
+        {
+            test: /\.css$/i,
+            use: initStyleLoaders({ isModules: false, isPreprocessor: false }),
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            exclude: /\.module.s[ac]ss$/,
+            use: initStyleLoaders({ isModules: false, isPreprocessor: true }),
+        },
+        {
+            test: /\.module\.s[ac]ss$/i,
+            use: initStyleLoaders({ isModules: true, isPreprocessor: true }),
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+            type: isProd ? "asset" : "asset/resource",
+            generator: {
+                filename: `static/media/img/${getAssetFilename()}`,
+            },
+        },
+        {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+            generator: {
+                filename: `static/media/fonts/${getAssetFilename()}`,
+            },
+        },
+    ];
+}
+
+function initStyleLoaders({ isModules, isPreprocessor }) {
     const loaders = [MiniCssExtractPlugin.loader];
 
-    if (config.isModules) {
+    if (isModules) {
         loaders.push({
             loader: "css-loader",
             options: {
@@ -34,138 +112,35 @@ function initStyleLoaders(config) {
         },
     });
 
-    if (config.isPreprocessor) {
+    if (isPreprocessor) {
         loaders.push("sass-loader");
     }
 
     return loaders;
 }
 
-module.exports = {
-    mode: isDev ? "development" : "production",
-    devtool: isDev ? "source-map" : undefined,
-    entry: "./src/index.tsx",
-    output: {
-        filename: `static/js/${filename("js")}`,
-        path: path.resolve(__dirname, "build"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html"),
-            filename: "index.html",
-            publicPath: "/",
+function initMinimizer() {
+    return [
+        new ImageMinimizerPlugin({
+            minimizer: {
+                implementation: ImageMinimizerPlugin.imageminMinify,
+                options: {
+                    plugins: [
+                        ["gifsicle", { interlaced: true }],
+                        ["jpegtran", { progressive: true }],
+                        ["optipng", { optimizationLevel: 5 }],
+                        ["svgo", { name: "preset-default" }],
+                    ],
+                },
+            },
         }),
-        new MiniCssExtractPlugin({
-            filename: `static/css/${filename("css")}`,
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts)|(tsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: { plugins: ["postcss-preset-env"] },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.module\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            modules: {
-                                localIdentName:
-                                    "[name]__[local]__[hash:base64:5]",
-                            },
-                        },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: { plugins: ["postcss-preset-env"] },
-                        },
-                    },
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                exclude: /\.module.s[ac]ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: { plugins: ["postcss-preset-env"] },
-                        },
-                    },
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-                type: isProd ? "asset" : "asset/resource",
-                generator: {
-                    filename: `static/media/img/${assetFilename()}`,
-                },
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: `static/media/fonts/${assetFilename()}`,
-                },
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminMinify,
-                    options: {
-                        plugins: [
-                            ["gifsicle", { interlaced: true }],
-                            ["jpegtran", { progressive: true }],
-                            ["optipng", { optimizationLevel: 5 }],
-                            ["svgo", { name: "preset-default" }],
-                        ],
-                    },
-                },
-            }),
-        ],
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        plugins: [
-            new TsconfigPathsPlugin({
-                extensions: [".js", ".jsx", ".ts", ".tsx"],
-            }),
-        ],
-    },
-    devServer: {
-        hot: true,
-        port: 3000,
-        open: true,
-        client: {
-            logging: "error",
-        },
-        historyApiFallback: true,
-    },
-};
+    ];
+}
+
+function getFilename(ext) {
+    return isDev ? `main.${ext}` : `main.[contenthash].${ext}`;
+}
+
+function getAssetFilename() {
+    return isDev ? "[name][ext]" : "[name].[hash][ext]";
+}
